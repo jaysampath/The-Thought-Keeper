3b8a45797ce5c93afe430c58e09f594761542643
Jay <50858217+jaysampath@users.noreply.github.com>
2021-06-03 01:40:54 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$link = mysqli_connect("sdb-e.hosting.stackcp.net","secretdiary_db-313833c85e","X@9K*5'[`YG,","secretdiary_db-313833c85e");
+  
+  if(mysqli_connect_error()){
+      die("Error while connecting to a database");
+  }
+  
+  ?>
